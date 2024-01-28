77b38596553ac960e464edbb2b2f8319cea78cb5
Danesnote <tyxmfh@naver.com>
2024-01-29 00:21:26 +0900

    jdbc setting

--- src/test/java/com/dane/webflux/WebfluxApplicationTests.java
+++ src/test/java/com/dane/webflux/WebfluxApplicationTests.java
@@ -3,6 +3,7 @@ package com.dane.webflux;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// 랜덤포트 사용으로 테스트시 메인과 포트 충돌 방지
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebfluxApplicationTests {
 
