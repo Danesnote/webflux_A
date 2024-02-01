3bf14545eae8a8c1a09f3bdb0c6c54349d77839e
danekim <tyxmfh@naver.com>
2024-02-01 21:41:34 +0900

    domain 작업 중, 테이블 구조 고민

--- src/main/java/com/dane/webflux/domain/post/Post.java
+++ src/main/java/com/dane/webflux/domain/post/Post.java
@@ -1,4 +1,21 @@
 package com.dane.webflux.domain.post;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("post")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    @Id
+    private Long id;
+
+
 }
